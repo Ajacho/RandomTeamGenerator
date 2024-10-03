bca4bd82820df99b5e5403d742dd353244e744e2
Belen Jacho <belen.jacho18@gmail.com>
2024-10-03 10:23:25 -0700

    Created home page, and TeamForm

---
+++ RandomTeamGenerator/Models/TeamFormation.cs
@@ -0,0 +1,6 @@
+namespace RandomTeamGenerator.Models{
+    public class TeamFormation {
+        public List<string> Names {get; set;} = new List<string>();
+        public string? TeamSize {get; set;}
+    }
+}
