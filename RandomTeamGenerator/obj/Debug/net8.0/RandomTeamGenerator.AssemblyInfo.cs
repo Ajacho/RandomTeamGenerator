ed9c7212ca3481e369dde903917e9ba1664ef47d
Belen Jacho <belen.jacho18@gmail.com>
2024-10-12 10:52:47 -0700

    need to add unit test

--- RandomTeamGenerator/obj/Debug/net8.0/RandomTeamGenerator.AssemblyInfo.cs
+++ RandomTeamGenerator/obj/Debug/net8.0/RandomTeamGenerator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RandomTeamGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e8787a9bab485e02aca3643ea2a44d2414435d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b44af7531fcf2577abe212a9e5d1aef5f493f7b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RandomTeamGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RandomTeamGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
